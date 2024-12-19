585ee7aa49d4c796ceb77ff6e7d395408ab391db
Christelle Ridad <sobbinginmycupofcoffee@gmail.com>
2024-12-19 14:51:42 +0800

    changed in auth the actual variables

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,7 +1,7 @@
-const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+const CLIENT_ID = "456b35c25847443aaf4a6da5719a85a1";
+const CLIENT_SECRET = "261c8b83f432490baae638ea860ce632";
+const REDIRECT_URL_AFTER_LOGIN = "https://mixtape-me.web.app/home";
 const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
-const REDIRECT_URL_AFTER_LOGIN = process.env.REACT_APP_REDIRECT_URL;
 const SPACE_DELIMETER = "%2b";
 const SCOPES = [
 
